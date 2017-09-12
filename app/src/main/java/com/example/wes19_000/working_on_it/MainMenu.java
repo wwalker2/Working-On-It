01209f85acaa0ad1d35f59713629206d48f5cc42
wwalker2 <wwalker2@xula.edu>
2017-09-12 17:29:26 -0500

    Commit 2

--- app/src/main/java/com/example/wes19_000/working_on_it/MainMenu.java
+++ app/src/main/java/com/example/wes19_000/working_on_it/MainMenu.java
@@ -2,12 +2,25 @@ package com.example.wes19_000.working_on_it;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.DatePicker;
+import android.widget.Toast;
 
 public class MainMenu extends AppCompatActivity {
 
+    DatePicker datePicker;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_menu);
+        datePicker = (DatePicker)findViewById(R.id.datePicker2);
+    }
+
+    public void pickDate(View view){
+        int day = datePicker.getDayOfMonth();
+        int month = datePicker.getMonth();
+        int year = datePicker.getYear();
+
+        Toast.makeText(getApplicationContext(), day + "/" + month + "/" + year, Toast.LENGTH_LONG).show();
     }
 }
