8ffc73b50b5b054f607bb06d48e7bd618a05434e
wwalker2 <wwalker2@xula.edu>
2017-09-20 13:46:23 -0500

    Add more text fields to DateDisplay

--- app/src/main/java/com/example/wes19_000/working_on_it/DateDisplay.java
+++ app/src/main/java/com/example/wes19_000/working_on_it/DateDisplay.java
@@ -29,6 +29,9 @@ public class DateDisplay extends AppCompatActivity {
         EditText addressText = (EditText)findViewById(R.id.clientAddress);
         EditText phoneText = (EditText)findViewById(R.id.clientPhone);
         EditText jobText = (EditText)findViewById(R.id.jobDescription);
+        EditText payText = (EditText)findViewById(R.id.payment);
+        EditText startDate = (EditText)findViewById(R.id.startDate);
+        EditText endDate = (EditText)findViewById(R.id.endDate);
 
         JobEntry job = new JobEntry();
 
@@ -36,5 +39,8 @@ public class DateDisplay extends AppCompatActivity {
         job.setClientAddress(addressText.getText().toString());
         job.setClientPhone(phoneText.getText().toString());
         job.setJobDescription(jobText.getText().toString());
+        job.setJobPay(Double.parseDouble(payText.getText().toString()));
+        job.setStartDate(startDate.getText().toString());
+        job.setEndDate(endDate.getText().toString());
     }
 }
